adf1d6831323907c45fb2821378b43fe370a85bc
MakerSquare Student <students@themakersquare.com>
2013-11-21 11:12:01 -0600

    roll and score methods added along with testing for a 0 score game

---
+++ bowling_game.rb
@@ -0,0 +1,14 @@
+class BowlingGame
+	attr_accessor :rolls
+
+	def initialize
+		@rolls = []
+	end
+
+	def roll(pins)
+	end
+	
+	def score
+		0
+	end
+end
---
+++ spec/bowling_game_spec.rb
@@ -0,0 +1,26 @@
+	require 'rubygems'
+	require 'bundler/setup'
+	require 'rspec'
+	require_relative '../bowling_game.rb'
+
+	describe BowlingGame do
+	 	describe '.initialize' do
+	 		it 'sets @rolls to an empty array' do
+	 			game = BowlingGame.new
+
+	 			expect(game.rolls).to eq([])
+	 		end
+	 	end
+
+
+	  describe '#score' do
+	    it 'scores the game as 0 when only gutter balls are thrown' do
+	      game = BowlingGame.new
+	      20.times { game.roll(0) }
+
+	      expect(game.score).to eq(0)
+	    end
+	  end
+	  
+	end
+	
