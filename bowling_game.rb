309f372e761ca7d664a42526be503b1941fe1fa5
MakerSquare Student <students@themakersquare.com>
2013-11-22 08:36:13 -0600

    Refactored out the code so it was cleaner and created some private methods in the process

--- bowling_game.rb
+++ bowling_game.rb
@@ -3,6 +3,8 @@ class BowlingGame
 
 	def initialize
 		@rolls = []
+		@total_score = 0
+		@current_roll = 0
 	end
 
 	# Record a roll in the game.
@@ -15,25 +17,37 @@ class BowlingGame
 	end
 
 	def score
-	  total_score  = 0
-	  current_roll = 0
+	  while @current_roll < @rolls.size
+	  	init_roll
 
-	  while current_roll < @rolls.size
-	    roll      = @rolls[current_roll]
-	    next_roll = @rolls[current_roll + 1]
-
-	    if roll == 10
-	      total_score += 10 + next_roll + @rolls[current_roll + 2]
-	      current_roll += 1
-	    elsif roll + next_roll == 10
-	      total_score += 10 + @rolls[current_roll + 2]
-	      current_roll += 2
+	    if strike?
+	      score_strike
+	    elsif @roll + @next_roll == 10
+	      @total_score += 10 + @rolls[@current_roll + 2]
+	      @current_roll += 2
 	    else
-	      total_score += roll + next_roll
-	      current_roll += 2
+	      @total_score += @roll + @next_roll
+	      @current_roll += 2
 	    end
 	  end
 
-	  return total_score
+
+	  return @total_score
+	end
+
+	private
+
+	def init_roll
+		@roll      = @rolls[@current_roll]
+		@next_roll = @rolls[@current_roll + 1]
+	end
+
+	def strike?
+		@roll == 10
+	end
+
+	def score_strike
+		@total_score += 10 + @next_roll + @rolls[@current_roll + 2]
+		@current_roll += 1
 	end
 end
